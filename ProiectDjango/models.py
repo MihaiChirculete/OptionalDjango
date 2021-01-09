b720efa950cb6e0fcda4645a92b3e2f486b15682
Chalapa13 <mihai.chirculete98@gmail.com>
2021-01-09 21:14:49 +0200

    Added models for blog Article and Comment

--- ProiectDjango/models.py
+++ ProiectDjango/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    author = models.EmailField(max_length=100)  # Who created this article
+    title = models.CharField(max_length=100)  # Title of the article
+    body = models.CharField(max_length=5000)  # Content of the article
+    date = models.DateTimeField('date published')  # Date when the article was published
+
+
+class Comment(models.Model):
+    author = models.EmailField(max_length=100)  # Who left this comment
+    body = models.CharField(max_length=400)  # Comment body
+    date = models.DateTimeField('date commented')  # Date when the comment was written
