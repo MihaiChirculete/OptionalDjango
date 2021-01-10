5085ad7a2e62980562be60f772aec09853a51787
Chalapa13 <mihai.chirculete98@gmail.com>
2021-01-10 10:45:18 +0200

    Added contact view.

--- ProiectDjango/views.py
+++ ProiectDjango/views.py
@@ -1,5 +1,15 @@
 from django.http import HttpResponse
+from django.template import loader
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    template = loader.get_template('sample-page.html')
+    context = {
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def contact(request):
+    template = loader.get_template('contact.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
