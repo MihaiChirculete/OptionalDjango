5d9b5fa6484b5c7869bc838bdcbe34d0969bddf0
Chalapa13 <mihai.chirculete98@gmail.com>
2021-01-10 16:58:35 +0200

    Refactoring

--- ProiectDjango/views.py
+++ ProiectDjango/views.py
@@ -4,7 +4,7 @@ from django.views.generic import TemplateView
 
 
 class IndexView(TemplateView):
-    template_name = "sample-page.html"
+    template_name = "index.html"
 
 
 class ContactView(TemplateView):
