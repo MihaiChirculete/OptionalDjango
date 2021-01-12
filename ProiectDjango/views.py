3794565018f34e7db58f7a8bd52437705cc3ae97
Chalapa13 <mihai.chirculete98@gmail.com>
2021-01-12 11:58:59 +0200

    Refactored and added class based view for Article list

--- ProiectDjango/views.py
+++ ProiectDjango/views.py
@@ -1,11 +1,42 @@
 from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
 from django.template import loader
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+from .models import Article
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
-class IndexView(TemplateView):
-    template_name = "index.html"
+# def article_list(request):
+#     object_list = Article.published.all()
+#     paginator = Paginator(object_list, 3)  # 3 posts in each page
+#     page = request.GET.get('page')
+#     try:
+#         posts = paginator.page(page)
+#     except PageNotAnInteger:
+#         # If page is not an integer deliver the first page
+#         posts = paginator.page(1)
+#     except EmptyPage:
+#         # If page is out of range deliver last page of results
+#         posts = paginator.page(paginator.num_pages)
+#     return render(request,
+#                   'ProiectDjango/article/list.html',
+#                   {'page': page,
+#                    'posts': posts})
 
 
-class ContactView(TemplateView):
-    template_name = "contact.html"
+def article_detail(request, year, month, day, post):
+    post = get_object_or_404(Article, slug=post,
+                             status='published',
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
+    return render(request,
+                  'ProiectDjango/article/detail.html',
+                  {'post': post})
+
+
+class ArticleListView(ListView):
+    queryset = Article.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'ProiectDjango/article/list.html'
