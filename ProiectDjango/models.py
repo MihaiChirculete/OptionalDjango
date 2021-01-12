e31964167f5b9a9908562e8499a5b14e146f55bf
Chalapa13 <mihai.chirculete98@gmail.com>
2021-01-12 10:29:17 +0200

    Added getter for absolute url for Article

--- ProiectDjango/models.py
+++ ProiectDjango/models.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User
+from django.urls import reverse
 from django.utils import timezone
 from django.db import models
 
@@ -33,7 +34,13 @@ class Article(models.Model):
         return self.title
 
     objects = models.Manager()  # The default manager
-    published = PublishedArticleManager()   # Custom manager defined above
+    published = PublishedArticleManager()  # Custom manager defined above
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                       args=[self.publish.year,
+                             self.publish.month,
+                             self.publish.day, self.slug])
 
 
 class Comment(models.Model):
