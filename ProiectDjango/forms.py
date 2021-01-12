227de30862ea23ed6f85ed87e118313cbb1543c1
Chalapa13 <mihai.chirculete98@gmail.com>
2021-01-12 12:25:39 +0200

    Added EmailArticle form

--- ProiectDjango/forms.py
+++ ProiectDjango/forms.py
@@ -1,1 +1,9 @@
 from django import forms
+
+
+class EmailArticleForm(forms.Form):
+    name = forms.CharField(max_length=25)
+    email = forms.EmailField()
+    to = forms.EmailField()
+    comments = forms.CharField(required=False,
+                               widget=forms.Textarea)
