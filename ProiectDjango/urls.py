310a905191d8005bc73ef0e67a4846a767dfea9f
Chalapa13 <mihai.chirculete98@gmail.com>
2021-01-12 13:02:25 +0200

    Added share Article view

--- ProiectDjango/urls.py
+++ ProiectDjango/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('<int:year>/<int:month>/<int:day>/<slug:post>/',
          views.article_detail,
          name='post_detail'),
+    path('<int:post_id>/share/',
+         views.article_share, name='post_share'),
 ]
--- ProiectDjango/views.py
+++ ProiectDjango/views.py
@@ -4,6 +4,8 @@ from django.template import loader
 from django.views.generic import TemplateView, ListView
 from .models import Article
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from .forms import EmailArticleForm
+from django.core.mail import send_mail
 
 
 # def article_list(request):
@@ -40,3 +42,29 @@ class ArticleListView(ListView):
     context_object_name = 'posts'
     paginate_by = 3
     template_name = 'ProiectDjango/article/list.html'
+
+
+def article_share(request, article_id):
+    # Retrieve article by id
+    article = get_object_or_404(Article, id=article_id, status='published')
+    sent = False
+
+    if request.method == 'POST':
+        # Form was submitted
+        form = EmailArticleForm(request.POST)
+        if form.is_valid():
+            # Form fields passed validation
+            clean_data = form.cleaned_data
+            article_url = request.build_absolute_uri(
+                article.get_absolute_url())
+            subject = f"{clean_data['name']} recommends you read " \
+                      f"{article.title}"
+            message = f"Read {article.title} at {article_url}\n\n" \
+                      f"{clean_data['name']}\'s comments: {clean_data['comments']}"
+            send_mail(subject, message, 'admin@proiectdjango.com',
+                      [clean_data['to']])
+            sent = True
+        else:
+            form = EmailArticleForm()
+            return render(request, 'ProiectDjango/article/share.html', {'post': article,
+                                                            'form': form})
