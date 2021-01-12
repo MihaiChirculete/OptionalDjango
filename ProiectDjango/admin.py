bd38b7405bd4b58fef065d202727a880b782e09a
Chalapa13 <mihai.chirculete98@gmail.com>
2021-01-12 10:07:53 +0200

    Changed the way that the Article model is being displayed on the admin page.

--- ProiectDjango/admin.py
+++ ProiectDjango/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
 
-from . import models
+from .models import Article
+from .models import Comment
 
-admin.site.register(models.Article)
-admin.site.register(models.Comment)
+admin.site.register(Comment)
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'author', 'publish', 'status')
+    list_filter = ('status', 'created', 'publish', 'author')
+    search_fields = ('title', 'body')
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ('author',)
+    date_hierarchy = 'publish'
+    ordering = ('status', 'publish')
