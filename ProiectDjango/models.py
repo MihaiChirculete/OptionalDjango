b8ea628aa12ddf4ffcb87986f0e88bc6a498e534
Chalapa13 <mihai.chirculete98@gmail.com>
2021-01-12 11:40:20 +0200

    Commented out absolute url getter of Article

--- ProiectDjango/models.py
+++ ProiectDjango/models.py
@@ -36,11 +36,11 @@ class Article(models.Model):
     objects = models.Manager()  # The default manager
     published = PublishedArticleManager()  # Custom manager defined above
 
-    def get_absolute_url(self):
-        return reverse('blog:post_detail',
-                       args=[self.publish.year,
-                             self.publish.month,
-                             self.publish.day, self.slug])
+    # def get_absolute_url(self):
+    #     return reverse('ProiectDjango:post_detail',
+    #                    args=[self.publish.year,
+    #                          self.publish.month,
+    #                          self.publish.day, self.slug])
 
 
 class Comment(models.Model):
